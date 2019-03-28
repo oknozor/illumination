ce537d9ef35c80ce6fb2228990bbb68824caa1cd
okno <paul.delafosse@protonmail.com>
2019-03-28 18:47:21 +0100

    poc

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,15 +1,26 @@
-use std::process;
+use std::{process, thread};
+use std::sync::{Arc, mpsc, Mutex, RwLock};
+use std::sync::atomic::AtomicBool;
+use std::sync::mpsc::channel;
+
 use gtk;
 use gtk::*;
-use super::header::Header;
-use super::content::Content;
-use std::sync::Arc;
-use std::sync::atomic::AtomicBool;
-use crate::preview::render;
 use neovim_lib::Neovim;
 use neovim_lib::Session;
 use sourceview::Buffer;
 use webkit2gtk::*;
+
+use crate::preview::render;
+
+use super::content::Content;
+use super::header::Header;
+use crate::nvim::handler::NvimHandler;
+use std::fs::File;
+use std::io::Write;
+use fragile::Fragile;
+use glib::spaced_primes_closest;
+use pango::lookup_aliases;
+use std::rc::Rc;
 
 pub struct App {
     pub window: Window,
@@ -18,6 +29,14 @@ pub struct App {
 }
 
 pub struct ConnectedApp(App);
+
+impl ConnectedApp {
+    /// Display the window, and execute the gtk main event loop.
+    pub fn then_execute(self) {
+        self.0.window.show_all();
+        gtk::main();
+    }
+}
 
 impl<'app> App {
     pub fn new() -> App {
@@ -42,31 +61,16 @@ impl<'app> App {
             Inhibit(false)
         });
 
-/*
-        content.preview.load_html(&render(&buffer), None);
-*/
-
         App { window, header, content }
     }
 
-/*    pub fn connect_events(self) -> ConnectedApp<'app> {
-        {
-            // Connect all of the events that this UI will act upon.
-            self.editor_changed(get_current_buffer(self.nvim));
-        }
-
-        // Wrap the `App` within `ConnectedApp` to enable the developer to execute the program.
-        ConnectedApp(self)
-    }*/
-
-/*    fn editor_changed(&self, buffer: String) {
-        let preview = self.content.preview.clone();
-        let buffer = Buffer::();
-        self.content.buffer.connect_changed(move |last_buffer| {
-            if buffer != self.content.buffer {
-                preview.load_html(&render(&buffer), None);
-                self.content.buffer = buffer;
-            }
+    pub fn connect_events(self) {
+        let preview = Arc::new(Mutex::new(Fragile::new(self.content.preview.clone())));
+        thread::spawn(move || {
+            let mut nvim_handler = NvimHandler::new();
+            nvim_handler.revc(Arc::clone(&preview));
         });
-    }*/
+
+    }
+
 }
