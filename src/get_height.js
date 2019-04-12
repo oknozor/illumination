04ff02791ff6f747e00870b00cba6ea373bb32ea
okno <paul.delafosse@protonmail.com>
2019-04-12 08:32:38 +0200

    add some debugging info for scrolling impl

--- src/get_height.js
+++
@@ -1,6 +0,0 @@
-function get_line_number () {
-    var lineHeight = document.body.style.lineHeight;
-    var scrollHeight = document.body.scrollHeight;
-    document.body.style.height = scrollHeight; // this is just for showing purposes
-    return Math.floor( scrollHeight / lineHeight );
-}
