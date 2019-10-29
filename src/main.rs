09773ff7e38981d1126167a1e2fd794073fbb776
Paul Delafosse <paul.delafosse@protonmail.com>
2019-10-29 14:33:59 +0100

    refactor(log): move logger to a dedicated module

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ mod nvim;
 mod preview;
 mod settings;
 mod ui;
+mod logger;
 
 use gtk::*;
 use ui::App;
@@ -31,29 +32,4 @@ fn main() {
     gtk::main();
 }
 
-// A simple logger only available if compiled with debug attribute
-mod logger {
-    use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
 
-    pub struct SimpleLogger;
-
-    pub static LOGGER: SimpleLogger = SimpleLogger;
-
-    impl log::Log for SimpleLogger {
-        fn enabled(&self, metadata: &Metadata) -> bool {
-            metadata.level() <= Level::Info
-        }
-
-        fn log(&self, record: &Record) {
-            if self.enabled(record.metadata()) {
-                println!("{} - {}", record.level(), record.args());
-            }
-        }
-
-        fn flush(&self) {}
-    }
-
-    pub fn init() -> Result<(), SetLoggerError> {
-        log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info))
-    }
-}
