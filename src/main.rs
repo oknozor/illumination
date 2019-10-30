4f72238121206df108f37fc7fa9eccea873fa8a1
Paul Delafosse <paul.delafosse@protonmail.com>
2019-10-30 12:44:52 +0100

    refactor: move nvim plugin to standard plugin dir

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,60 @@ extern crate horrorshow;
 extern crate log;
 #[macro_use]
 extern crate lazy_static;
+extern crate clap;
+use clap::{App, Arg};
 
 extern crate config;
 extern crate dirs;
 
 mod doc_lang;
 mod html;
+mod logger;
 mod nvim;
 mod preview;
 mod settings;
 mod ui;
-mod logger;
 
 use gtk::*;
-use ui::App;
+use ui::App as GtkApp;
 
 fn main() {
     logger::init().expect("Error initializing logger");
     info!("Illumination started in debud mode");
+
     #[cfg(debug_assertions)]
     settings::show();
 
-    let app = App::new();
-    app.window.show_all();
-    app.connect_nvim();
+    let version = env!("CARGO_PKG_VERSION");
+    let author = env!("CARGO_PKG_AUTHORS");
+
+    let matches = App::new("Illumination cli")
+        .version(version)
+        .author(author)
+        .about("Does awesome things")
+        .arg(
+            Arg::with_name("standalone")
+                .long("standalone")
+                .short("s")
+                .value_name("INPUT")
+                .takes_value(true)
+                .help("Open a single html/mardown file in headless mode"),
+        )
+        .get_matches();
+
+    println!("starting illumination");
+
+    if matches.is_present("standalone") {
+        let app = GtkApp::new();
+        let path = matches.value_of("standalone").unwrap();
+        println!("Using input file: {}", path);
+        app.window.show_all();
+        app.standalone_mode(path)
+    } else {
+        let app = GtkApp::new();
+        app.window.show_all();
+        app.connect_nvim();
+    }
+
     gtk::main();
 }
-
-
