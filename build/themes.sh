bc0f9afb476beb6e65aa1bcc41deca6206ad40be
Paul Delafosse <paul.delafosse@protonmail.com>
2019-10-30 15:26:40 +0100

    fix: remove custom build

--- build/themes.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p .local/share/nvim/site/plugin/
-mv neovim/illumination .local/share/nvim/site/plugin/illumination.vim
-mkdir -p themes/default
-
-curl -X GET https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js > themes/default/hljs.min.js
-curl -X GET https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/rust.min.js > themes/default/hljs-rust.js
-curl -X GET https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/default.min.css > themes/default/hljs.min.css
-curl -X GET https://gist.githubusercontent.com/ryangray/1882525/raw/2a6e53f645b960f0bed16d686ba3df36505f839f/buttondown.css > themes/default/style.css
-curl -X GET https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css > themes/github.css
