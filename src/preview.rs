1f0c985eebab930126809e1aa3c9a8992daee3e8
Paul Delafosse <paul.delafosse@protonmail.com>
2024-05-16 13:10:26 +0200

    feat: add mathjax js deps

--- src/preview.rs
+++ src/preview.rs
@@ -30,6 +30,31 @@ pub fn render(markdown: &str, scroll: f64) -> String {
         scroll
     );
 
+    let math = r#"
+    <script
+      src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+      type="text/javascript">
+    </script>
+    <script type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML">
+    </script>
+    <script type="text/x-mathjax-config">
+      MathJax.Hub.Config({
+        tex2jax: {
+          inlineMath: [['$','$'], ['\\(','\\)']],
+          processEscapes: true},
+          jax: ["input/TeX","input/MathML","input/AsciiMath","output/CommonHTML"],
+          extensions: ["tex2jax.js","mml2jax.js","asciimath2jax.js","MathMenu.js","MathZoom.js","AssistiveMML.js", "[Contrib]/a11y/accessibility-menu.js"],
+          TeX: {
+          extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"],
+          equationNumbers: {
+          autoNumber: "AMS"
+          }
+        }
+      });
+    </script>
+    "#;
+
     format!(
         "{}",
         html!(
@@ -42,6 +67,7 @@ pub fn render(markdown: &str, scroll: f64) -> String {
                         : Raw(HLJS_CSS.as_str());
                         : Raw(THEME.lock().unwrap().contents.as_str());
                     }
+                    : Raw(math);
                     script {
                         : Raw(JS.as_str());
                     }
